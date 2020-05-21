f9b26380683832404671d6dac644b2456c2321cf
김상우 <namacoel@gmail.com>
2020-05-21 21:56:54 +0900

    MODULE - 도형의 넓이 예제 적용

--- MODULE/mathUtil.js
+++ MODULE/mathUtil.js
@@ -1,12 +1,18 @@
 const PI = 3.14;
-const getCircleArea = r=>r*r*PI;
+const getCircleArea = (r) => r * r * PI;
+const getSquareArea = (d) => d * d;
+
+module.exports = {
+  PI,
+  getCircleArea,
+  getSquareArea,
+};
 
 // exports.PI = PI;
 
 // exports.getCircleArea = getCircleArea;
 
-
-export default {
-    PI,
-    getCircleArea
-}
+// export default {
+//     PI,
+//     getCircleArea
+// }
